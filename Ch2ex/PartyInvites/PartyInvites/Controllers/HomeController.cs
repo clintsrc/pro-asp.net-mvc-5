5d476c88da903a7c3fccda5690956e38dfac6dac
Clint Jones <clinton.alan.jones@gmail.com>
2026-06-23 16:30:39 -0700

    Examples 2.8-10

--- Ch2ex/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ Ch2ex/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,76 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Dynamic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Claims;
+using System.Security.Policy;
+using System.Text.RegularExpressions;
 using System.Web;
-using System.Web.Mvc;   // Controller base class
-
-// MVC: Models (data and business logic), Views (UI HTML rendering), Controllers (routes traffic: receives input
-//  requests, fetches from the Models, forwards to Views for rendering).
-//  The Razor engine renders the View
-
-// IIS Express is a lightweight version of IIS that ships with VisualStudio to run and test web apps locally.
-//  Just click the IIS Express button with the browser fo your choice selected. It automatically stops when you close the browser.
-//  IIS assigns the app a random port when you create the project (e,g, https://localhost:44343/ in .vs/<app_name>/config/applicationhost.config)
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
 
 
-// Controllers handle incoming requests: they are C# classes that inherit from the built-in Controller base class.
 namespace PartyInvites.Controllers
 {
-    // Follow the convention to use NameController
-    // Visual Studio automaticaly handles some of the code functionality as long as conventions are followed.
-    // To add new Controllers to the project: Solution Explorer: Right-click Controllers, Add | Controller
-    
-    // Home[Controller] is the default controller that supports these routes by default:
-    //  /
-    //  /Home
-    //  /Home/Index
-
     public class HomeController : Controller
     {
-        // Action method: public method invoked by a web URL to perform an action
 
-        // 2.1
-        // This is a stripped down version of the template's default action method.
-        // public ActionResult Index()
-        // {
-        //     return View();
-        // }
+        // 2.8 (see the Home/Index.cshtml file)
 
+        // 2.9 The model (or domain model) represents real world objects, processes, and rules that define the subject (domain) of the app.
+        //  It contsins C# domain objects  that define the domain and the methods that manipulate them.
+        //  Views and Controllers present the domain to the clients. An MVC app starts with a well-designed model to which controllers and views are added.
+        //
+        //  Model classes are in the Models folder by convention
+        //  To create a new model in Solution Explorer: Right-click Models | Add | Class
+        //  File name: GuestResponse.cs
+        //  Add to create the class.
+        //  (See the Models\GuestResponse.cs file)
+        //
+        // 2.10 see Views\Home\Index.cshtml
 
-        // 2.2
-        // This example returns a string instead of HTML
-        // public string Index()
-        // {
-        //     return "Hello World";
-        // }
-
-
-        // 2.3
-        // 2.4-5 (see the Home/Index.cshtml file)
-        // A view provides an HTML response
-        // To add Views to Action methods: Right-click method name directly in the code editor, Add View
-        //   Common | MVC | View, Add
-        //   View Name: Follow the convention to set this to the name of the action method that the view will be associated with (Index here)
-        //   Template: set to Empty (without model)
-        //   Options:
-        //   Create as a partial view: disabled (default)
-        //   Use a layout page: disabled
-        //   Click Add
-        // This will create the new View file at: Views/Home/Index.cshtml
-        // The convention is that the view has the name of the action method and is contained in a folder named after the controller: /Views/Home/Index.cshtml
-        // public ViewResult Index()
-        // {
-        //     // The View method returns a ViewResult
-        //     return View();
-        // }
-
-
-        // 2.6
-        // 2.7 (see the Home/Index.cshtml file)
-        // The ViewBag object takes arbitrary properties and values that are passed from the controller to a view.
-        //   It's a little like localStorage, but it's server-side and only persistent for the life of the request.
+        // This example object stores, validates, and confirms RSVPs
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
@@ -83,3 +47,6 @@ namespace PartyInvites.Controllers
     }
 
 }
+
+
+
---
+++ Ch2ex/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+
+    // 2.9 The model (or domain model)
+    // 2.10 see Views\Home\Index.cshtml
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        // WillAttend is a nullable boolean property (bool?). It can be true, false, or null
+        public bool? WillAttend { get; set; }
+    }
+}
