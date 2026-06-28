0acb94b28f7548d521aee7787ca52dab8365548b
Clint Jones <clinton.alan.jones@gmail.com>
2026-06-27 17:02:36 -0700

    Examples 2.12-19

--- Ch2ex/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ Ch2ex/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ using System.Web.UI.WebControls;
 using System.Xml.Linq;
 using Antlr.Runtime.Misc;
 using Microsoft.Ajax.Utilities;
-using PartyInvites.Models;
+using PartyInvites.Models;  // 2.14 import the models to simplify reference to the GuestResponse class
 using static System.Collections.Specialized.BitVector32;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -32,37 +32,57 @@ namespace PartyInvites.Controllers
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
-            
+
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
 
             return View();
         }
 
+        // 2.12-13 see Views/Home/RsvpForm.cshtml
 
-        // 2.11
-        // An Action method here for /Home/RsvpForm view
-        //
-        // To create a strongly typed view for the RsvpForm Action method:
-        //
-        // *** Build the project first *** to ensure the GuestResponse class model is compiled for the strongly typeed view to use
-        //
-        // Solution Explorer: Right-click RsvpForm method here in the code editor:
-        // Add | Add View
-        // View Name: RsvpForm
-        // Template: Empty   (NOTE: not 'Empty (without model)')
-        // Model Class: GuestResponse
-        // Leave the View Options options disabled (default)
-        // Add to create the view
-        //
-
-        // 2.12-13 (see Views/Home/RvspForm.cshtml)
+        // 2.14
+        // By default a form will handle both GET and POST HTTP requests:
+        //   when the form recieves a POST response it simply calls the RsvpForm action method again 
+        //   and reloads the same view.
+        //   
+        [HttpGet]   // Now the RsvpForm action method will only specifically handle GET requests
         public ViewResult RsvpForm()
         {
             return View();
         }
+
+        // This overloaded RsvpForm method will only handle POST requests
+        // It recieves the guestResponse data and applies the HttpPost attribute.
+        // You need to create this Thanks (as before, right-click one of the methods in the code editor):
+        // Add | Add View
+        // View Name: Thanks
+        // Template: Empty   (NOTE: not 'Empty (without model)')
+        // Model Class: GuestResponse
+        // Leave the View Options options disabled (default)
+        // Add to create the view
+
+        // This post method will now:
+        // - find the render the "Thanks" view.
+        // - pass the GuestResponse object to the view
+
+        // 2.15-16 see Views/Home/Thanks.cshtml
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            // 2.17
+            // Check for input validation errors
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else {
+                // report validation errors
+
+                // 2.18 see Views/Home/RsvpForm.cshtml
+                return View();
+            }
+
+        }
     }
 
 }
-
-
-
--- Ch2ex/PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ Ch2ex/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -2,18 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;    // import declarative for standard validation rules
 
 namespace PartyInvites.Models
 {
 
-    // 2.9 The model (or domain model)
-    // 2.10 see Views\Home\Index.cshtml
+    // 2.16
+    // Input validation is applied in the domain model instead of the UI so that
+    //   validation is handled in one place, and applied wherever the model is used.
+
+    // 2.17 (see Controllers/HomeControllers.cshtml)
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")] // MVC detects these and automatically validate data during the model-binding process
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
-        // WillAttend is a nullable boolean property (bool?). It can be true, false, or null
+
+        // A nullable boolean ("bool?") can be true, false or null.
+        // The value will be null, which will trigger a validation error unless
+        //   the user makes a selection.
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
         public bool? WillAttend { get; set; }
     }
 }
